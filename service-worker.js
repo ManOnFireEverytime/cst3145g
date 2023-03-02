fd01191fdaf45bcb3ec97e3a3981560669d1574d
ManOnFireEverytime <manonfireeverytime@gmail.com>
2023-03-02 02:30:44 +0000

    vcx

--- service-worker.js
+++ service-worker.js
@@ -22,31 +22,31 @@ self.addEventListener("install", function (e) {
   );
 });
 
-// self.addEventListener("fetch", function (e) {
-//   e.respondWith(
-//     caches.match(e.request).then(function (cachedFile) {
-//       //download the file if it is not in the cache
-//       if (cachedFile) {
-//         console.log(
-//           " [Service Worker] Resource fetched from the cache for: " +
-//             e.request.url
-//         );
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (cachedFile) {
+      //download the file if it is not in the cache
+      if (cachedFile) {
+        console.log(
+          " [Service Worker] Resource fetched from the cache for: " +
+            e.request.url
+        );
 
-//         return cachedFile;
-//       } else {
-//         return fetch(e.request).then(function (response) {
-//           return caches.open(cacheName).then(function (cache) {
-//             //add the new file to the cache
-//             cache.put(e.request, response.clone());
-//             console.log(
-//               "[Service Worker] Resource fetched and saved in the cache for:" +
-//                 e.request.url
-//             );
+        return cachedFile;
+      } else {
+        return fetch(e.request).then(function (response) {
+          return caches.open(cacheName).then(function (cache) {
+            //add the new file to the cache
+            cache.put(e.request, response.clone());
+            console.log(
+              "[Service Worker] Resource fetched and saved in the cache for:" +
+                e.request.url
+            );
 
-//             return response;
-//           });
-//         });
-//       }
-//     })
-//   );
-// });
+            return response;
+          });
+        });
+      }
+    })
+  );
+});
